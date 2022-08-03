4e85cb836b907748fbe2ca0427a199a0f43c37c9
v7153623 <71223238+v7153623@users.noreply.github.com>
2022-08-03 19:29:34 +0900

    Update b10828_stack.java

--- inhyeok/0803/b10828_stack.java
+++ inhyeok/0803/b10828_stack.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class b10828_stack {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
